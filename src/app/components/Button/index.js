bd4b1385c32cfecf67562cf9cc0a932e67c56efc
rachosuar <rachosuar@gmail.com>
2022-10-17 15:52:39 -0300

    cambio boton tweetitem

--- src/app/components/Button/index.js
+++ src/app/components/Button/index.js
@@ -1,7 +1,27 @@
 import "./styles.css";
+import GradientSpinner from "../loaders/GradientSpinner";
 
 const Button = (props) => {
-  return <button className="button" {...props}></button>;
+  const { children, loading, className, onClick } = props;
+  const fullClassName = ["button"];
+
+  if (className) {
+    fullClassName.push(className);
+  }
+  if (loading) {
+    fullClassName.push("loading");
+  }
+
+  return (
+    <button
+      {...props}
+      onClick={!loading ? onClick : undefined}
+      className={fullClassName.join(" ")}
+    >
+      {loading && <GradientSpinner size={30} />}
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- src/app/pages/Home/components/TweetItem/index.js
+++ src/app/pages/Home/components/TweetItem/index.js
@@ -3,19 +3,22 @@ import moment from "moment";
 import "moment/locale/es";
 import "./styles.css";
 import Button from "../../../../components/Button";
+import autosize from "../../../../../utils/libs/autosize.min";
 
 const RELATIVE_TIME_UPDATE = 1000 * 60;
 
 const TweetItem = ({
   body,
-  createMode,
+  createMode = false,
   created_at,
   user,
-  error,
+  onChange,
   onPublish,
   loading,
+  error,
 }) => {
   const [bodyValue, setBodyValue] = useState("");
+  const [bodyFocus, setBodyFocus] = useState(false);
   moment.locale("es");
   const [relativeTime, setRelativeTime] = useState(
     moment(created_at).fromNow()
@@ -24,6 +27,11 @@ const TweetItem = ({
     () => setRelativeTime(moment(created_at).fromNow()),
     RELATIVE_TIME_UPDATE
   );
+
+  useEffect(() => {
+    autosize(document.querySelector(".tweet-container textarea"));
+  }, []);
+
   useEffect(() => {
     return () => {
       clearInterval(intervalId);
@@ -33,7 +41,11 @@ const TweetItem = ({
   if (!user) return;
 
   return (
-    <div className="tweet-container">
+    <div
+      className={`tweet-container${bodyFocus ? " focus" : ""} ${
+        error ? " error" : ""
+      }`}
+    >
       <div className="tweet-header">
         <img
           src={user.photo?.base64 ?? user.photo}
@@ -42,28 +54,35 @@ const TweetItem = ({
         <span>{user.name}</span>
       </div>
       {createMode ? (
-        <>
-          <textarea
-            className="tweet-body"
-            placeholder="Escribe algo..."
-            onChange={(e) => setBodyValue(e.target.value)}
-          ></textarea>
-          {error ? <span className="error-message">{error}</span> : null}
-        </>
+        <textarea
+          disabled={loading}
+          className="tweet-body"
+          placeholder="Escribe algo..."
+          value={bodyValue}
+          onFocus={() => setBodyFocus(true)}
+          onBlur={() => setBodyFocus(false)}
+          onChange={(e) => {
+            setBodyValue(e.target.value);
+            onChange?.(e);
+          }}
+        ></textarea>
       ) : (
         <div className="tweet-body">
           <span>{body}</span>
         </div>
       )}
       <div className="tweet-footer">
+        <div>{error && <span className="error-msg">{error}</span>}</div>
         {createMode ? (
           <Button
+            className="publish-button"
+            loading={loading}
             onClick={async () => {
               const success = await onPublish?.({ body: bodyValue, user });
               if (success) setBodyValue("");
             }}
           >
-            {loading ? "Publicando" : "Publicar"}
+            {!loading ? "Publicar" : "Publicando"}
           </Button>
         ) : (
           <span>{relativeTime}</span>
